08f51745a82b04d10ba1e312ad34ce3313f4b2ff
kspgale <katie.gale@dfo-mpo.gc.ca>
2018-08-03 14:07:19 -0700

    Changed to incorporate species matrices (depth bin x species) instead of long-mode dataframe

--- JoinSpatialPoints.R
+++ JoinSpatialPoints.R
@@ -18,22 +18,15 @@ library(rgdal)
 setwd("E:/Documents/Projects/WorldClass_DiveSurveys/RCP/bhm_dive_data_prep/Data/")
 
 #Import data, join depth category,  and add SourceKey to join to spatial file
-specobs<-read.csv("UpdatedObservations/SpeciesObs_updated.csv")
-specobs<-specobs[,c(-1,-2)]
-specobs$HKey_Quad<-paste(specobs$HKey, specobs$Quadrat, sep="_")
+inv<-read.csv("SpeciesBy_matrices/InvertsByDepthCategory.csv")
+algae<-read.csv("SpeciesBy_matrices/InvertsByDepthCategory.csv")
+names(inv)[2:ncol(inv)]<-paste0("I_", names(inv)[2:ncol(inv)])
+names(algae)[2:ncol(algae)]<-paste0("A_", names(algae)[2:ncol(algae)])
 
-quadrat<-read.csv("ExtractedData/quadrat.csv")
-quadrat$HKey_Quad<-paste(quadrat$HKey, quadrat$Quadrat, sep="_")
-quadrat<-quadrat[,names(quadrat) %in% c("HKey_Quad", "DepthCat")]
-
-#join depth category to each quadrat
-length(unique(specobs$HKey_Quad)) #10055
-length(unique(quadrat$HKey_Quad)) #11097 (probably because there are some quadrats without species)
-
-specrec<-merge(specobs, quadrat, by="HKey_Quad", all.x=T, all.y=F)
-
-#Add identifier to join to spatial points
-specrec$SourceKey<-paste("S16",  specrec$HKey, specrec$DepthCat, sep="_")
+#Combine algae and inverts into a single dataframe. Can be separated out later if wanted.
+summary(inv$TransDepth==algae$TransDepth) #all True
+specrec<-cbind.data.frame(inv, algae[2:ncol(algae)])
+specrec$SourceKey<-paste0("S16_",specrec$TransDepth)
 
 #Bring in spatialized points files for both north central coast and Haida Gwaii
 spatpoint_NCC<-readOGR("E:/Documents/Projects/WorldClass_DiveSurveys/WCTSS_Dive_Db/SpatializedPoints/NCC/S11_FinalSpatializedPointsAll.shp")
@@ -58,16 +51,15 @@ spatpoint<-spatpoint[spatpoint$surv=="S16",]
 
 #Join species records to spatial points 
 spatWithSpp<-merge(specrec, spatpoint, by="SourceKey", all.x=T, all.y=F)
-nrow(specrec) #85916
-nrow(spatWithSpp) #85916
+nrow(specrec) #2838
+nrow(spatWithSpp) #2838
 
 #Check for problems
-length(unique(spatWithSpp$SourceKey[is.na(spatWithSpp$Lat)]))#60 depth bins (1111 records) are missing spatial points. This is likely due to errors in spatial point creation.
+length(unique(spatWithSpp$SourceKey[is.na(spatWithSpp$Lat)]))#57 depth bins are missing spatial points. This is likely due to errors in spatial point creation.
 
-length(unique(specrec$SourceKey)) #The species data has 2856 depth bins
+length(unique(specrec$SourceKey)) #The species data has 2838 depth bins
 length(unique(spatpoint$SourceKey)) #The spatialized points have 2811 depth bins of WC dives
-#So there are maybe 47 (2858-2811) WC depth bins that are not represented in the spatialized points. Might be sites with no species data. 
-
+#So there are maybe 27 (2838-2811) WC depth bins that are not represented in the spatialized points. Might be sites with no species data. 
 
 #convert to shapefile and write
 spatWithSpp_shp<-subset(spatWithSpp[!is.na(spatWithSpp$Lon),])
